8b697e64867c1061397f9b9d015d6615af6f7384
Pat Spence <patspenceMAC@MacBook-2.local>
2016-09-14 19:01:12 -0600

    Commit

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>Home</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+	<?php include 'header.php'; ?>
+	<h1> Welcome To Aerial Headhunters</h1>
+	<h2> InsertNumberUsers have found a job using Aerial HeadHunters </h2>
+	<?php include 'footer.php'; ?>
  </body>
 </html>
