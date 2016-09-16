d4c6e9c1f0b9858becc1117a45eb62abb7092682
Pat Spence <patspenceMAC@MacBook-2.local>
2016-09-15 20:28:27 -0600

    added rectnagle to styles.css

--- index.php
+++ index.php
@@ -6,7 +6,14 @@
  <body>
 	<?php include 'header.php'; ?>
 	<h1> Welcome To Aerial Headhunters</h1>
-	<h2> InsertNumberUsers have found a job using Aerial HeadHunters </h2>
+
+	<div id="rectangle">
+		<div id="inner">
+		InsertNumberUsers have found a job using Aerial HeadHunters
+		</div>
+	</div>
+
+
 	<?php include 'footer.php'; ?>
  </body>
 </html>
