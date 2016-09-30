d0a3703311347f0f3f1bdb5aa4ea6b1e5b48e5af
patspence <patspence@u.boisestate.edu>
2016-09-30 16:25:33 -0600

    deleted desciption

--- about.php
+++ about.php
@@ -28,15 +28,7 @@
 	
 	<div class ="centerContainer">
 	<h1> ABOUT Aerial Headhunters:</h1>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat dui non commodo efficitur. Phasellus auctor id nibh vel efficitur. In lobortis risus lorem, vitae vulputate mi tempor at. Sed metus dolor, feugiat sit amet tempus sit amet, cursus a risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec magna libero, convallis nec risus nec, sollicitudin laoreet ligula. Sed eget rhoncus turpis. Nam eu odio eget justo molestie ullamcorper. Nullam porttitor mollis felis, sed lacinia libero scelerisque sagittis. Donec iaculis porta tempus. Donec egestas diam eget mi interdum eleifend. Duis nec nisi nec felis lacinia vulputate.
 
-Aenean et nunc a turpis fringilla pulvinar ut eget neque. Fusce ac porttitor lacus, a tincidunt velit. Morbi sagittis faucibus nibh, et feugiat ante vulputate id. Suspendisse et consectetur urna. Quisque sed nulla eget mi consequat ultrices eu non lorem. Quisque hendrerit ac est in eleifend. Praesent at consequat tellus. Donec lobortis mauris sed mattis placerat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam ultricies risus a ipsum ornare, in lobortis arcu elementum. In magna mauris, lobortis et arcu mattis, auctor blandit dolor. In rhoncus tincidunt ligula in ultrices.
-
-Ut consectetur feugiat ligula, id pretium turpis tempor et. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque quis orci ut tellus pretium accumsan. Quisque odio metus, vehicula in pellentesque vel, tempus vel sapien. Phasellus sed blandit nisl. Suspendisse quis iaculis purus. Donec non cursus sapien, a euismod ante. Pellentesque consectetur, ligula ac finibus tempus, nibh mi feugiat velit, et ultricies tortor sem sed nisl. Ut pharetra mi faucibus risus consequat, sed tempus tortor rhoncus. Nullam tempus, odio sit amet sagittis molestie, metus lectus sagittis ante, et posuere nunc enim eget arcu.
-
-Integer lobortis ipsum libero. Sed sit amet convallis magna. Curabitur non tincidunt augue, quis porta turpis. In hac habitasse platea dictumst. Aenean blandit, nibh sed mattis tempus, libero massa sodales nibh, ut rhoncus purus libero ac justo. Maecenas a velit eu massa mattis porta sit amet ut turpis. Morbi vel sollicitudin nibh. Mauris sem purus, vestibulum eget consequat ut, dignissim et nunc. Etiam at quam ut dolor rutrum laoreet vitae eu metus. Aliquam erat volutpat. Nunc auctor ullamcorper auctor. Sed lacinia interdum augue eu auctor. Ut sollicitudin blandit accumsan. Sed malesuada libero a laoreet efficitur.
-
-Morbi vel suscipit arcu. Etiam dictum magna at dui facilisis, ut ultrices leo maximus. Suspendisse laoreet lacus in ultrices tincidunt. Donec sed orci id nibh tincidunt consequat. Proin suscipit tristique laoreet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris egestas vestibulum leo, consequat accumsan magna eleifend vitae.
 	</div>
 
 	<div class="footer">
