30efb63ec57f30d442e2e4c21574c8671b2bd915
Pat Spence <patspenceMAC@MacBook-2.local>
2016-10-03 21:37:43 -0600

    added logo

--- headernorectangle.php
+++ headernorectangle.php
@@ -1,14 +1,17 @@
 
 <div id = "navbar">
 		<ul class="toolbar2">
-			<div class ="toolbarLeft">
+			<div class ="companyName">
+				<li class = "companyName">Aerial Head Hunters</li>
+
+				<img class="companyIcon" src="favicon/favicon-32x32.png">
+			</div>
+			<div class ="HAB">
 				<li class="toolbar"><a href="index.php">Home</a></li>
 				<li class ="toolbar"><a href="about.php">About</a></li>
 				<li class ="toolbar"><a href="browse.php">Browse</a></li>
-				<li class ="toolbar"><a href="droneProfile.php">Temporary link for droneProfile</a></li>
-				<li class ="toolbar"><a href="seekerProfile.php">Temporary link for seekerProfile</a></li>
 			</div>
-			<div class ="toolbarRight">
+			<div class ="LS">
 				<li class ="toolbar"><a href="login.php">Login</a></li>
 				<li class ="toolbar"><a href="signup.php">Signup</a></li>
 			</div>
