914ca5e47b89d6f0d47ba8c538923414620e3928
patspence <patspence@u.boisestate.edu>
2016-10-03 23:26:13 -0600

    Fixed underline in links

--- headernorectangle.php
+++ headernorectangle.php
@@ -2,12 +2,11 @@
 <div id = "navbar">
 		<ul class="toolbar2">
 			<div class ="companyName">
-				<li class = "companyName">Aerial Head Hunters</li>
-
+				<a href="index.php"><li class = "companyName">Aerial Head Hunters</a></li>
 				<img class="companyIcon" src="favicon/favicon-32x32.png">
 			</div>
 			<div class ="HAB">
-				<li class="toolbar"><a href="index.php">Home</a></li>
+
 				<li class ="toolbar"><a href="about.php">About</a></li>
 				<li class ="toolbar"><a href="browse.php">Browse</a></li>
 			</div>
