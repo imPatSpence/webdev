cdc6bbd36c07c57614cce87364da00497090a8ba
imPatSpence <impatspence@gmail.com>
2016-11-26 21:58:32 -0700

    fixed file upload

--- scripts/imageUploadHandler.php
+++ scripts/imageUploadHandler.php
@@ -4,23 +4,19 @@
   $db = new dao();
   $accountName = $_SESSION["authed_user"];
 
-  // save a product, including name, description, and an image path
+
   $location = (isset($_POST["location"])) ? $_POST["location"] : "";
   $description = (isset($_POST["description"])) ? $_POST["description"] : "";
-  $fileName = $_FILES["file"]["location"];
+  $fileToUpload = $_FILES["fileToUpload"]["name"];
   
-  $target_dir = "/images";
 
-  
   $dest = $_SERVER['DOCUMENT_ROOT'];
-  $finalPath= $dest.$target_dir.$fileName;
+  $finalPath= $dest."/images/".$fileToUpload;
   
 if ( move_uploaded_file ( $_FILES["fileToUpload"]["tmp_name"], $finalPath ) )
    echo "Download completed";
 else
    echo "Error";
 
-//move_uploaded_file ($_FILES['fileToUpload'] ['tmp_name'], 
- //      "/uploads/{$_FILES['fileToUpload'] ['name']}")
 
   ?>
