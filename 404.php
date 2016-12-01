6ff363f27449715f4ffa25f5d8bc1213a03b0d5e
Patrick <impatspence@gmail.com>
2016-11-30 17:00:17 -0700

    added 404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php require_once 'headernorectangle.php'; 
+session_start();
+
+?>
+
+PAGE DOES NOT EXIST
+<div class="footer">
+		<div id="rectangleFooter">
+			<?php require_once 'footer.php'; ?>
+		</div>
+	</div>
