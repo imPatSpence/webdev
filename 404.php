efdc3ae3f9594abee985c03364fc65fb03de42be
Patrick <impatspence@gmail.com>
2016-11-30 17:02:05 -0700

    added 404 styling

--- 404.php
+++ 404.php
@@ -2,8 +2,39 @@
 session_start();
 
 ?>
+<html>
+<head>
+<link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="manifest" href="favicon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+  <title>404</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link rel="stylesheet" type="text/css" href="styles/droneProfile2.css">
+	 	<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+	<script src="js/jquery-3.1.1.js"></script>
+	<script src="/js/PgwSlider-master/pgwslider.js"></script>
+	<link href="/js/PgwSlider-master/pgwslider.min.css" rel="stylesheet" type="text/css">
+
+ </head>
+ <body>
 
 PAGE DOES NOT EXIST
+</body>
 <div class="footer">
 		<div id="rectangleFooter">
 			<?php require_once 'footer.php'; ?>
